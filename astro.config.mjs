aac948cfdef4a3162648afa609ad0839b9a83749
Kashmeer Jamirie <kash@Kashmeers-MacBook-Air.local>
2026-01-09 14:43:06 +0800

    Update site URL to 11raja.com and admin to strapi.11raja.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://cms.11raja.com',
+	site: 'https://11raja.com',
 	integrations: [mdx(), sitemap()],
 });
